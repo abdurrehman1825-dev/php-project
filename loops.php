dc983d2edb7b1602541297ace781393b8904b0d2
abdurrehman1825-dev <abdurrehman1825.dev@gmail.com>
2025-03-19 14:23:10 +0500

    while loop and for each loop on array

--- loops.php
+++ loops.php
@@ -1,4 +1,19 @@
 <?php
+//while loop
+$count = 0;
+while($count <= 10){
+    echo "pakistan zindabad <br/>";
+    $count++;
+}
 
+$arr = array(1,2,3,4,5,6,7,8,9);
+$value_initial = 0;
+while($value_initial < count($arr)){
+    echo $arr[$value_initial];
+    $value_initial++;
+}
 
+/* foreach($arr as $newValue){
+    echo $newValue;
+} */
 ?>
