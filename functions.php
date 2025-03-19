0d824c91b0bbc14760899e4c1494b47eea8dd4c7
abdurrehman1825-dev <abdurrehman1825.dev@gmail.com>
2025-03-19 15:01:26 +0500

    fucntion modify with local and globle scop

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php 
+define("name","abdur rehman khan "); // constan and can access inside a funtion
+//const name = "Abdur rehman"; can access inside a funtion
+// $name = "Abdur rehman";  can not be accessed inside a funtion
 function add_two(int $first,float $secnd): float{ // we can set return type 
+    echo name;
     return $first + $secnd;
 }
 
