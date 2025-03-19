089fc4e0c981a4b68ec28209f6ce019f60c78dc0
abdurrehman1825-dev <abdurrehman1825.dev@gmail.com>
2025-03-19 21:20:03 +0500

    this key word added

--- oopClassObject.php
+++ oopClassObject.php
@@ -11,7 +11,7 @@ $boltair->age = 5;
 $boltair->sound="wo wo";
 var_dump($boltair) */
 
-class Test{
+/* class Test{
     public function __construct (public $name){}
 
     public function show(){
@@ -21,6 +21,24 @@ class Test{
 
 $newName = new Test("Abdur Rehman");
 //$newName->name = "Abdur Rehman";
-$newName->show();
+$newName->show(); */
+class Employee {
+    private $id;
+    private $name;
+    private $designation;
 
+    public function set_employee($id,$name,$designation){
+        $this->id = $id;
+        $this->name=$name;
+        $this->designation=$designation;
+    }
+    public function get_employee(){
+        echo $this->id;
+        echo $this->name;
+        echo $this->designation;
+    }
+}
+$zahid = new Employee();
+$zahid->set_employee(2,"zahid","system engineer");
+$zahid->get_employee();
 ?>
